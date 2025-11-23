4788b93b23fdb7fa8a10281e15ca98d2ba3a85c7
Abhishek Kumar <sagargraphics2020@gmail.com>
2025-11-24 00:23:50 +0530

    Add start script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source env/bin/activate
-export PORT=${PORT:-8000}
 python manage.py collectstatic --noinput
-gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
+python manage.py collectstatic --noinput
+gunicorn core.wsgi:application --timeout 120 --workers 3
