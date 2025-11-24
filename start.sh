4cf4459a4191ff4a125cf28671dc97f3d9c931d1
Abhishek Kumar <sagargraphics2020@gmail.com>
2025-11-25 00:09:09 +0530

    Update Database

--- start.sh
+++ start.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
+# gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
+
+#!/usr/bin/env bash
+# Activate virtual environment (Render uses .venv)
+source .venv/bin/activate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate
+
+# Start gunicorn
 gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
